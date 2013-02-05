b38becf90308609210b53ed33ec2f70ee60d824f
joeoakes <joe.oakes@gmail.com>
2013-02-05 16:20:36 -0500

    Added Image object

--- Flicker/FlickrClient.cs
+++ Flicker/FlickrClient.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+
 
 namespace ChessByBird.FlickerProject
 {
     class FlickrClient
     {
+         
+        Image newImage = Image.FromFile("SampImag.jpg");
 
         public static bool GetFlickerPic()
         {
