aedb65cf93901320caaa70dde1bbab3025ea693d
joeoakes <joe.oakes@gmail.com>
2013-02-05 16:12:01 -0500

    Added method stubs

--- Flicker/FlickrClient.cs
+++ Flicker/FlickrClient.cs
@@ -8,5 +8,15 @@ namespace ChessByBird.FlickerProject
 {
     class FlickrClient
     {
+
+        public static bool GetFlickerPic()
+        {
+            return true;
+        }
+
+        public static bool PostFlickerPic()
+        {
+            return true;
+        }
     }
 }
