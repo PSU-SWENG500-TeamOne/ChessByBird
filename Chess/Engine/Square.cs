993d9f0c1d39c970e34b268ebe3829964fe6f7c8
a-gene <a.eugene.jr@gmail.com>
2013-02-05 21:55:35 -0500

    additions to chess implemen

---
+++ Chess/Engine/Square.cs
@@ -0,0 +1,16 @@
+namespace ChessProject.Engine
+{
+    internal struct Square
+    {
+        internal Piece Piece;
+
+        #region Constructors
+
+        internal Square(Piece piece)
+        {
+            Piece = new Piece(piece);
+        }
+
+        #endregion
+    }
+}
