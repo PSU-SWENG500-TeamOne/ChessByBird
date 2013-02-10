8dd9ac8ceee16cb6a9f77e15ffbe892b2583d3a7
Zach Carson <z.carson@gmail.com>
2013-02-10 10:13:41 -0500

    Basic Functionality
    When run, program can authernticate against twitter, then go and get the
    first couple mentions and output the details of them. after doing all
    that, it creates a new tweet with a generic message.
    
    Still needs to have tests written into it.

--- Twitter/Program.cs
+++ Twitter/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace ChessByBird.TwitterProject
 {
@@ -14,9 +17,56 @@ namespace ChessByBird.TwitterProject
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            var oauth = GetOAuthInfo();
+            var twitter = new TinyTwitter(oauth);
+            var startingtime = DateTime.Now;
+            
+
+            //while (true)
+            //{
+            var myMentions = twitter.GetMentions(300328033800306680, 20);
+                myMentions.ToList().ForEach(
+                    x =>
+                    {
+                        string moveString = "not found";
+                        
+                        if (x.Text.Contains("*"))
+                        {
+                            int startIndex = x.Text.IndexOf("*");
+                            int endIndex = x.Text.LastIndexOf("*");
+                            moveString = x.Text.Substring(startIndex, endIndex-startIndex+1);
+                        }
+
+                        Console.WriteLine("Now gathering info about tweet #" + x.Id.ToString() +".");
+                        Console.WriteLine("  --  The sender of this tweet was '" + x.ScreenName + " and it was in reply to " + x.inReplyToID + "'.");
+                        Console.WriteLine("  --  The text of this tweet was: '" + x.Text + "'.");
+                        Console.WriteLine("  --  The move attached to this tweet was " + moveString + "'.");
+                        
+                    }
+                );
+            Console.WriteLine("This concludes the current mentions");
+
+            var status = Guid.NewGuid().ToString();
+            twitter.UpdateStatus(status);
+
+           // }
+
+        }
+
+        
+        
+        private static OAuthInfo GetOAuthInfo()
+        {
+            System.Reflection.Assembly _assembly = Assembly.GetExecutingAssembly();
+            System.IO.Stream _xmlStream = _assembly.GetManifestResourceStream("ChessByBird.TwitterProject.oauth-info.xml");
+
+            var serializer = new XmlSerializer(typeof(OAuthInfo));
+            return (OAuthInfo)serializer.Deserialize(_xmlStream);
+
         }
     }
 }
