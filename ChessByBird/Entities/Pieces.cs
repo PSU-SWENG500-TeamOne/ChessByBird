f7c4a5c2a4fd20402141cb755b880215f3429c40
joeoakes <joe.oakes@gmail.com>
2013-02-19 21:18:11 -0500

    Integrated Entities into CBB

---
+++ ChessByBird/Entities/Pieces.cs
@@ -0,0 +1,33 @@
+﻿/********************************************
+ *  Penn State University Software Engineering Graduate Program
+ *  Authors: Team 1: Zachary Carson, Aaron Eugene, Steve Haggerty, Joseph Oakes
+ *  Date: Spring 2013
+ *  Course: SWENG 500 Software Engineering Studio
+ *  Professor: Mohamad Kassab
+ *  Project: Chess By Bird Capstone group project
+*********************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessByBird.ClassLibraryProject
+{
+    class Pieces
+    {
+        public enum chessPieceColor { white, black }
+        public enum chessPieceType { King, Queen, Rook, Bishop, Knight, Pawn, None}
+        
+        //TODO: Finish constructor method
+        public Pieces()
+        {
+        }
+
+        //TODO: Finish method
+        public void GetPieceType()
+        {
+        }
+    }
+}
