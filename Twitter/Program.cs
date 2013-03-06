40d7bc17330561065e37e8d681225dd7351c93ae
Zach Carson <z.carson@gmail.com>
2013-03-06 18:25:29 -0500

    Removed legacy twitter api junk

--- Twitter/Program.cs
+++ Twitter/Program.cs
@@ -75,62 +75,6 @@ namespace ChessByBird.TwitterProject
                 }
             );
             Console.WriteLine("End of new API stuff");
-            
-            
-            
-            
-            
-            
-            var oauth = GetOAuthInfo();
-            var twitter = new TinyTwitter(oauth);
-            var startingtime = DateTime.Now;
-            
 
-
-            //while (true)
-            //{
-            var myMentions = twitter.GetMentions(300328033800306680, 20);
-                myMentions.ToList().ForEach(
-                    x =>
-                    {
-                        string moveString = "not found";
-                        
-                        if (x.Text.Contains("*"))
-                        {
-                            int startIndex = x.Text.IndexOf("*");
-                            int endIndex = x.Text.LastIndexOf("*");
-                            moveString = x.Text.Substring(startIndex, endIndex-startIndex+1);
-                        }
-
-                        Console.WriteLine("Now gathering info about tweet #" + x.Id.ToString() +".");
-                        Console.WriteLine("  --  The sender of this tweet was '" + x.ScreenName + " and it was in reply to " + x.inReplyToID + "'.");
-                        Console.WriteLine("  --  The text of this tweet was: '" + x.Text + "'.");
-                        Console.WriteLine("  --  The move attached to this tweet was " + moveString + ".");
-                        
-                    }
-                );
-            Console.WriteLine("This concludes the current mentions");
-
-            //var tweet = twitter.GetSpecificTweet(299722128025063425);
-
-
-            var status = Guid.NewGuid().ToString();
-            twitter.UpdateStatus(status);
-
-           // }
-
-        }
-
-        
-        
-        private static OAuthInfo GetOAuthInfo()
-        {
-            System.Reflection.Assembly _assembly = Assembly.GetExecutingAssembly();
-            System.IO.Stream _xmlStream = _assembly.GetManifestResourceStream("ChessByBird.TwitterProject.oauth-info.xml");
-
-            var serializer = new XmlSerializer(typeof(OAuthInfo));
-            return (OAuthInfo)serializer.Deserialize(_xmlStream);
-
-        }
     }
 }
